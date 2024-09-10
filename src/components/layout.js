0d5f23d347595a30e6667b985100ba44bec56ea0
Sam Bunce <sam.bunce@hotmail.com>
2024-09-10 13:31:00 +0100

    Contact page

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,4 +1,13 @@
+import { space } from "data/styles";
 import styled from "styled-components";
 
-export const H1 = styled.h1``;
+export const H1 = styled.h1`
+  font-size: 36pt;
+  margin: 0 0 ${space.box} 0;
+`;
+
+export const MassiveH1 = styled.h1`
+  font-size: 60pt;
+`;
+
 export const P = styled.p``;
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,11 +1,40 @@
 import pageWrapper from "HOCs/PageWrapper";
+import { MassiveH1 } from "components/layout";
+import { colors } from "data/styles";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
-const HomePage = () => {
+const CenteredContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const ContactLink = styled(Link)`
+  width: 250px;
+  padding: 2rem 0;
+  background-color: ${colors.highlight1};
+  color: ${colors.background};
+  text-decoration: none;
+  text-transform: uppercase;
+
+  &:active {
+    filter: brightness(150%);
+    transition: 0.4s;
+  }
+`;
+
+const ContactPage = () => {
   return (
-    <>
-      <p>Contact</p>
-    </>
+    <CenteredContent>
+      <MassiveH1>Contact</MassiveH1>
+      <ContactLink type="button" to="mailto:sam.bunce@hotmail.com">
+        Email
+      </ContactLink>
+    </CenteredContent>
   );
 };
 
-export default pageWrapper(HomePage);
+export default pageWrapper(ContactPage);
